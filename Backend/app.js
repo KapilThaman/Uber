0632a21231415840daed6995e54e5757ffb0d64f
KapilThaman <kapil.thaman.k@gmail.com>
2024-12-05 20:35:21 -0800

    User Registration Done using services which are just seperate file creating users using models

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const connectToDb = require("./DB/db");
 const app = express();
+const userRoutes = require("./Routes/user.routes");
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.send("Working");
 })
+
+app.use("/users",userRoutes);
 
 module.exports = app;
