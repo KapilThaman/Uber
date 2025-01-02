bfdc1fe083c5cc25769f3f85d4f96e57f59daaac
KapilThaman <kapil.thaman.k@gmail.com>
2025-01-01 20:48:35 -0800

    Done with location sample data added as well 050158

---
+++ Frontend/src/componants/LocationSearchPanel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function LocationSearchPanel() {
+  //sample array of locations
+  const locations = [
+    "Near 13438 central ave, Surrey Central",
+    "Near 13439 central ave, Surrey Central",
+    "Near 13440 central ave, Surrey Central",
+    "Near 13441 central ave, Surrey Central",
+  ];
+  return (
+    // This is just a sample data
+    <div>
+      {locations.map((item) => {
+        return (
+          <div className=" gap-4 border-2 p-3 border-gray-100 active:border-black rounded-lg   flex items-center my-2 justify-start">
+            <h2 className="bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full">
+              <i className="ri-map-pin-fill"></i>
+            </h2>
+            <h4 className="font-medium">
+              {item}
+            </h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default LocationSearchPanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,174 +1,139 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../componants/LocationSearchPanel';
 function Home() {
   const [ pickup, setPickup ] = useState('')
   const [ destination, setDestination ] = useState('')
   const [ panelOpen, setPanelOpen ] = useState(false)
+  const [ pickupSuggestions, setPickupSuggestions ] = useState([])
+  const [ destinationSuggestions, setDestinationSuggestions ] = useState([])
 
+    const panelRef = useRef();
+    const panelCloseRef = useRef();
 
-  const submitHandler = (e) => {
+  const submitHandler = ()=>{
     e.preventDefault()
-}
-useGSAP(function () {
-  if (panelOpen) {
-      gsap.to(panelRef.current, {
-          height: '70%',
-          padding: 24
-          // opacity:1
-      })
-      gsap.to(panelCloseRef.current, {
-          opacity: 1
-      })
-  } else {
-      gsap.to(panelRef.current, {
-          height: '0%',
-          padding: 0
-          // opacity:0
-      })
-      gsap.to(panelCloseRef.current, {
-          opacity: 0
-      })
   }
+
+  useGSAP(function () {
+    if (panelOpen) {
+        gsap.to(panelRef.current, {
+            height: '70%',
+            padding: 24
+            // opacity:1
+        })
+        gsap.to(panelCloseRef.current, {
+            opacity: 1
+        })
+       
+    } else {
+        gsap.to(panelRef.current, {
+            height: '0%',
+            padding: 0
+            // opacity:0
+        })
+        gsap.to(panelCloseRef.current, {
+            opacity: 0
+        })
+        
+    }
 }, [ panelOpen ])
 
-// useGSAP(function () {
-//   if (vehiclePanel) {
-//       gsap.to(vehiclePanelRef.current, {
-//           transform: 'translateY(0)'
-//       })
-//   } else {
-//       gsap.to(vehiclePanelRef.current, {
-//           transform: 'translateY(100%)'
-//       })
-//   }
-// }, [ vehiclePanel ])
 
-// useGSAP(function () {
-//   if (confirmRidePanel) {
-//       gsap.to(confirmRidePanelRef.current, {
-//           transform: 'translateY(0)'
-//       })
-//   } else {
-//       gsap.to(confirmRidePanelRef.current, {
-//           transform: 'translateY(100%)'
-//       })
-//   }
-// }, [ confirmRidePanel ])
-
-// useGSAP(function () {
-//   if (vehicleFound) {
-//       gsap.to(vehicleFoundRef.current, {
-//           transform: 'translateY(0)'
-//       })
-//   } else {
-//       gsap.to(vehicleFoundRef.current, {
-//           transform: 'translateY(100%)'
-//       })
-//   }
-// }, [ vehicleFound ])
-
-// useGSAP(function () {
-//   if (waitingForDriver) {
-//       gsap.to(waitingForDriverRef.current, {
-//           transform: 'translateY(0)'
-//       })
-//   } else {
-//       gsap.to(waitingForDriverRef.current, {
-//           transform: 'translateY(100%)'
-//       })
-//   }
-// }, [ waitingForDriver ])
   return (
     <div className='h-screen relative overflow-hidden'>
     <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-    <div className='h-screen w-screen'>
-        {/* image for temporary use  */}
-        <LiveTracking />
-    </div>
-    <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='h-[30%] p-6 bg-white relative'>
-            <h5 ref={panelCloseRef} onClick={() => {
-                setPanelOpen(false)
-            }} className='absolute opacity-0 right-6 top-6 text-2xl'>
-                <i className="ri-arrow-down-wide-line"></i>
-            </h5>
-            <h4 className='text-2xl font-semibold'>Find a trip</h4>
-            <form className='relative py-3' onSubmit={(e) => {
-                submitHandler(e)
-            }}>
-                <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
-                <input
-                    onClick={() => {
-                        setPanelOpen(true)
-                        setActiveField('pickup')
-                    }}
+            <div className='h-screen w-screen'>
+                {/* image for temporary use  */}
+                <img className='h-full w-full object-cover' src="https://www.researchgate.net/publication/320839993/figure/fig3/AS:556713386676224@1509742222719/Map-in-Uber-application-tracking-user-in-a-Yellow-Cab.png" alt="" />
+                {/* <LiveTracking /> */}
+            </div>
+            <div className=' flex flex-col justify-end top-0 h-screen absolute w-full '>
+                <div className='h-[30%] pt-5 pl-5 pr-5 bg-white relative '>
+                <h5 ref={panelCloseRef} onClick={() => {
+                        setPanelOpen(false)
+                    }} className='absolute opacity-0 right-6 top-6 text-2xl'>
+                        <i className="ri-arrow-down-wide-line"></i>
+                    </h5>
+                <h4 className='text-2xl font-semibold'>Find a trip </h4>
+                <form onSubmit={(e)=>{submitHandler(e)}} className='relative py-3'>
+                    <div className="line w-1 h-16 absolute top-[20%] left-5 bg-gray-900 rounded-full" ></div>
+                    <input 
                     value={pickup}
-                    onChange={handlePickupChange}
-                    className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full'
-                    type="text"
-                    placeholder='Add a pick-up location'
-                />
-                <input
-                    onClick={() => {
-                        setPanelOpen(true)
-                        setActiveField('destination')
-                    }}
+                    onClick={()=>{setPanelOpen(true)}}
+                    className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full ' 
+                    type="text" 
+                    placeholder='Add a pick-up location' 
+                    onChange={(e)=>{setPickup(e.target.value)}}
+                    />
+                    <input 
                     value={destination}
-                    onChange={handleDestinationChange}
-                    className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
-                    type="text"
-                    placeholder='Enter your destination' />
-            </form>
-            <button
-                onClick={findTrip}
-                className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
-                Find Trip
-            </button>
-        </div>
-        <div ref={panelRef} className='bg-white h-0'>
-            <LocationSearchPanel
-                suggestions={activeField === 'pickup' ? pickupSuggestions : destinationSuggestions}
-                setPanelOpen={setPanelOpen}
-                setVehiclePanel={setVehiclePanel}
-                setPickup={setPickup}
-                setDestination={setDestination}
-                activeField={activeField}
-            />
-        </div>
+                    onClick={()=>{setPanelOpen(true)}}
+                    className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3' 
+                    type="text" 
+                    placeholder='Enter  your destination' 
+                    onChange={(e)=>{setDestination(e.target.value)}}
+                    />
+                </form>
+                </div>
+                <div ref={panelRef} className='h-[0%] bg-white'>
+                    <LocationSearchPanel/>
+                </div>
+                        
+            </div>
+            <div className='fixed w-full z-10 bottom-0  translate-y-full bg-white px-3 py-6'>
+            <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
+                <div className='flex border-2 mb-3 active:border-black  rounded-xl w-full items-center justify-between p-3'>
+                    <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_638/v1682350114/assets/c2/296eac-574a-4a81-a787-8a0387970755/original/UberBlackXL.png" alt="" />
+                    <div className='w-1/2'>
+                    <h4 className='font-medium text-base'>UberGo 
+                        <span>
+                            <i className="ri-user-3-fill">
+                            </i>
+                                4
+                        </span>
+                    <h5 className='font-medium text-sm'>2 mins away </h5>
+                    <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>  
+                    </h4>
+                    </div>
+                    <h2 className='text-lg font-semibold'>₹193</h2>
+                </div>
+                <div className='flex border-2 mb-3 active:border-black rounded-xl w-full items-center justify-between p-3'>
+                    <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+                    <div className='w-1/2'>
+                    <h4 className='font-medium text-base'>UberGo Bike 
+                        <span>
+                            <i className="ri-user-3-fill">
+                            </i>
+                                1
+                        </span>
+                    <h5 className='font-medium text-sm'>2 mins away </h5>
+                    <p className='font-normal text-xs text-gray-600'>Affordable, motor cycle rides</p>  
+                    </h4>
+                    </div>
+                    <h2 className='text-lg font-semibold'>₹65</h2>
+                </div>
+                <div className='flex border-2 mb-3 active:border-black rounded-xl w-full items-center justify-between p-3'>
+                    <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+                    <div className='w-1/2'>
+                    <h4 className='font-medium text-base'>UberGo Bike 
+                        <span>
+                            <i className="ri-user-3-fill">
+                            </i>
+                                1
+                        </span>
+                    <h5 className='font-medium text-sm'>2 mins away </h5>
+                    <p className='font-normal text-xs text-gray-600'>Affordable, motor cycle rides</p>  
+                    </h4>
+                    </div>
+                    <h2 className='text-lg font-semibold'>₹65</h2>
+                </div>
+                
+                
+            </div>
     </div>
-    <div ref={vehiclePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
-        <VehiclePanel
-            selectVehicle={setVehicleType}
-            fare={fare} setConfirmRidePanel={setConfirmRidePanel} setVehiclePanel={setVehiclePanel} />
-    </div>
-    <div ref={confirmRidePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
-        <ConfirmRide
-            createRide={createRide}
-            pickup={pickup}
-            destination={destination}
-            fare={fare}
-            vehicleType={vehicleType}
-
-            setConfirmRidePanel={setConfirmRidePanel} setVehicleFound={setVehicleFound} />
-    </div>
-    <div ref={vehicleFoundRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-12'>
-        <LookingForDriver
-            createRide={createRide}
-            pickup={pickup}
-            destination={destination}
-            fare={fare}
-            vehicleType={vehicleType}
-            setVehicleFound={setVehicleFound} />
-    </div>
-    <div ref={waitingForDriverRef} className='fixed w-full  z-10 bottom-0  bg-white px-3 py-6 pt-12'>
-        <WaitingForDriver
-            ride={ride}
-            setVehicleFound={setVehicleFound}
-            setWaitingForDriver={setWaitingForDriver}
-            waitingForDriver={waitingForDriver} />
-    </div>
-</div>
   )
 }
 
